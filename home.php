7752dd7ce9c291262cb76d8b9e5275c6f9be2619
jasky.ong.2017 <jasky.ong.2017@sis.smu.edu.sg>
2019-10-30 18:05:29 +0800

    add htaccess

---
+++ home.php
@@ -0,0 +1,14 @@
+<?php //header("Location: home.php");
+?>
+<html>
+  <head>
+    <title>Get Lucky</title>
+    <h1>Welcome to Get Lucky</h1>
+  </head>
+  <body>
+    ...
+    ...
+    ...
+    <a href="listview.html">List-view Page</a>
+  </body>
+</html>
