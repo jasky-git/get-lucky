59f7ded551d7a73d22f6c12acdf0f34450d58213
muhammady2017 <muhammady.2017@sis.smu.edu.sg>
2019-11-06 20:43:47 +0800

    update history and historyDAO pages

--- History.php
+++ History.php
@@ -1,18 +1,24 @@
 <?php
 
 class History {
-    private $id;
+    private $venueId;
     private $venue;
     private $address;
     private $lat;
     private $lng;
+    private $title;
+    private $date;
+    private $userId;
 
-    public function __construct($id,$venue,$address,$lat,$lng){
-        $this->id = $id;
+    public function __construct($venueId,$venue,$address,$lat,$lng,$title,$date,$userId){
+        $this->venueId = $venueId;
         $this->venue = $venue;
         $this->address = $address;
         $this->lat = $lat;
         $this->lng = $lng;
+        $this->title = $title;
+        $this->date = $date;
+        $this->userId = $userId;
     }
 }
 
--- HistoryDAO.php
+++ HistoryDAO.php
@@ -4,18 +4,21 @@ require_once 'ConnectionManager.php';
 require_once 'History.php';
 
 class HistoryDAO {
-    public function addIternary($id,$venue,$address,$lat,$lng){
-        $sql = "INSERT INTO history (id, venue, address, lat, lng) VALUES (:id, :venue, :address, :lat, :lng)";
+    public function addIternary($venueId,$venue,$address,$lat,$lng,$title,$date,$userId){
+        $sql = "INSERT INTO history (venueId, venue, address, lat, lng, title, date, userId) VALUES (:venueId, :venue, :address, :lat, :lng, :title, :date, :userId)";
 
         $connMgr = new ConnectionManager();      
         $conn = $connMgr->getConnection();
         $stmt = $conn->prepare($sql);
         
-        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+        $stmt->bindParam(':venueId', $venueId, PDO::PARAM_STR);
         $stmt->bindParam(':venue', $venue, PDO::PARAM_STR);
         $stmt->bindParam(':address', $address, PDO::PARAM_STR);
         $stmt->bindParam(':lat', $lat, PDO::PARAM_STR);
         $stmt->bindParam(':lng', $lng, PDO::PARAM_STR);
+        $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+        $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+        $stmt->bindParam(':userId', $userId, PDO::PARAM_STR);
 
         $isAddOK = False;
         if ($stmt->execute()) {
