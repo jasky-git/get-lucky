a0f6fb670b88ab65fcf7604b21f3751912dc66ff
jasky.ong.2017 <jasky.ong.2017@sis.smu.edu.sg>
2019-11-06 21:22:34 +0800

    update history and historydao

--- History.php
+++ History.php
@@ -1,13 +1,31 @@
 <?php
 
 class History {
-    private $id;
-    private $venue;
-    private $address;
-    private $lat;
-    private $lng;
+    public $historyid;
+    public $venueid;
+    public $userid;
+    public $venue;
+    public $title;
+    public $address;
+    public $lat;
+    public $lng;
+    public $datee;
 
-    public function __construct($id,$venue,$address,$lat,$lng){
+    public function __construct() {
+        $get_arguments = func_get_args();
+        $number_of_arguments = func_num_args();
+
+        if (method_exists($this, $method_name = '__construct'.$number_of_arguments)) {
+            call_user_func_array(array($this, $method_name), $get_arguments);
+        }
+    }
+
+    public function __construct2($datee,$venue) {
+        $this->datee = $datee;
+        $this->venue = $venue;
+    }
+
+    public function __construct5($id,$venue,$address,$lat,$lng){
         $this->id = $id;
         $this->venue = $venue;
         $this->address = $address;
--- HistoryDAO.php
+++ HistoryDAO.php
@@ -5,25 +5,82 @@ require_once 'History.php';
 
 class HistoryDAO {
     public function addIternary($id,$venue,$address,$lat,$lng){
-        $sql = "INSERT INTO history (id, venue, address, lat, lng) VALUES (:id, :venue, :address, :lat, :lng)";
+      $sql = "INSERT INTO history (id, venue, address, lat, lng) VALUES (:id, :venue, :address, :lat, :lng)";
 
-        $connMgr = new ConnectionManager();      
-        $conn = $connMgr->getConnection();
-        $stmt = $conn->prepare($sql);
-        
-        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
-        $stmt->bindParam(':venue', $venue, PDO::PARAM_STR);
-        $stmt->bindParam(':address', $address, PDO::PARAM_STR);
-        $stmt->bindParam(':lat', $lat, PDO::PARAM_STR);
-        $stmt->bindParam(':lng', $lng, PDO::PARAM_STR);
+      $connMgr = new ConnectionManager();
+      $conn = $connMgr->getConnection();
+      $stmt = $conn->prepare($sql);
 
-        $isAddOK = False;
-        if ($stmt->execute()) {
-            $isAddOK = True;
-        }
+      $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+      $stmt->bindParam(':venue', $venue, PDO::PARAM_STR);
+      $stmt->bindParam(':address', $address, PDO::PARAM_STR);
+      $stmt->bindParam(':lat', $lat, PDO::PARAM_STR);
+      $stmt->bindParam(':lng', $lng, PDO::PARAM_STR);
 
-        return $isAddOK;
+      $isAddOK = False;
+      if ($stmt->execute()) {
+          $isAddOK = True;
+      }
+
+      return $isAddOK;
+  }
+
+  public function getTimeLine(){
+    $sql = "SELECT venue,address,lat,lng FROM history";
+
+    $connMgr = new ConnectionManager();
+    $conn = $connMgr->getConnection();
+    $stmt = $conn->prepare($sql);
+
+    // $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+
+    $result = array();
+
+    //PDOStatement::fetch styles
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // while($row = $stmt->fetch()) {
+        // $result[] = new history($row['venue'], $row['address']);
+    // }
+
+    // clean up resources
+    //$connMgr->close($conn,$stmt);
+
+    return $result;
+  }
+
+  public function getAll(){
+    //WHERE USER = :USER
+    // $sql = "SELECT DISTINCT date, venue FROM history";
+    $sql = "SELECT DISTINCT date, venue FROM history";
+
+    $connMgr = new ConnectionManager();
+    $conn = $connMgr->getConnection();
+    $stmt = $conn->prepare($sql);
+
+    // $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+
+    $result = array();
+
+    //PDOStatement::fetch styles
+    // $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // $result[] = new history($row['date'], $row['venue']);
+        $result[] = new history($row['date'], $row['venue']);
     }
+
+    // clean up resources
+    //$connMgr->close($conn,$stmt);
+
+    return $result;
+  }
 }
 
 ?>
