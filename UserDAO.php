23e804e7c55e3d8e89321d9a3f66087139b9a562
jasky.ong.2017 <jasky.ong.2017@sis.smu.edu.sg>
2019-11-11 00:37:14 +0800

    fix minor conflicts and adding telegram confirmation process

--- UserDAO.php
+++ UserDAO.php
@@ -3,44 +3,33 @@ require_once 'ConnectionManager.php';
 require_once 'User.php';
 
 class UserDAO {
-    
-<<<<<<< HEAD
+
     public function getUser($name, $password) {
         $sql = 'select * from user where name=:name AND password=:password';
-=======
-    public function getUser($userid) {
-        $sql = 'select * from user where userid=:userid';
->>>>>>> parent of e723bf2... Update on login function
 
         $connMgr = new ConnectionManager();
         $conn = $connMgr->getConnection();
-        
+
         $stmt = $conn->prepare($sql);
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
-<<<<<<< HEAD
+
         $stmt->bindParam(':name', $name, PDO::PARAM_STR);
         $stmt->bindParam(':password', $password, PDO::PARAM_STR);
         $stmt->execute();
-        
+
         $user = array();
         while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             return new user($row['userid'], $row['name'], $row['email'], $row['tele_username'], $row['password']);
-=======
-        $stmt->bindParam(':userid', $userid, PDO::PARAM_STR);
-        $stmt->execute();
-        while($row = $stmt->fetch()) {
-            return new User($row['userid'], $row['password'], $row['name'], $row['email'], $row['telegram_username']);
->>>>>>> parent of e723bf2... Update on login function
         }
     }
 
     public function addUser($name, $email, $tele_username, $password){
         $sql = "INSERT INTO user (name, email, tele_username, password) VALUES (:name, :email, :tele_username, :password)";
 
-        $connMgr = new ConnectionManager();      
+        $connMgr = new ConnectionManager();
         $conn = $connMgr->getConnection();
         $stmt = $conn->prepare($sql);
-        
+
         $stmt->bindParam(':name', $name, PDO::PARAM_STR);
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
         $stmt->bindParam(':tele_username', $tele_username, PDO::PARAM_STR);
